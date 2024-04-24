89391bc174848a0f916a61700275715f52a00f40
Curt Arbtin <arbtin@gmail.com>
2024-04-23 21:21:51 -0500

    Create Shape.ts

---
+++ Shape.ts
@@ -0,0 +1,10 @@
+// Inheritance Example
+export class Shape {
+
+  constructor(private _x: number, private _y: number) {
+  }
+
+  public getInfo(): string {
+    return `x=${this._x} y=${this._y}`;
+  }
+}
